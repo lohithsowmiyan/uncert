965bbee83c6b677813a81eb852c0f8fd976f86cb
Lohith Senthilkumar <lohithsowmiyan1@gmail.com>
2025-05-21 04:42:26 -0400

    added eu and au but buggy

--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 from typing import Tuple, List, Dict, Any
 import random
 import math
+import numpy as np
+from sklearn.utils import resample
 
 class Model(ABC):
     """
@@ -30,9 +32,9 @@ class Model(ABC):
         pass
 
 
-class NaiveBayesModel(Model):
+class NaiveBayesModel():
     def __init__(self, start: int, stop : int, batch_per: float = 0):
-        super().__init__()
+        #super().__init__()
         self.model = GaussianNB()
         self.batch_per = batch_per
         self.scores = []
@@ -43,7 +45,7 @@ class NaiveBayesModel(Model):
         self.start = start
 
 
-    def fit(self, train: List[Tuple[List[float], Any]], test: List[Tuple[List[float], Any]], sample = 'uncertainity') -> None:
+    def fit_1(self, train: List[Tuple[List[float], Any]], test: List[Tuple[List[float], Any]], sample = 'uncertainity') -> None:
         self.test_set = test
         self.batch_size = max(1, int(len(train) * self.batch_per))
         shuffled = train.copy()
@@ -60,17 +62,18 @@ class NaiveBayesModel(Model):
             self.model.fit(X_done, y_done)
 
             # Get most uncertain sample
-            if sample == 'uncertainty':
-                most_uncertain, *self.todo = self.calculate_uncertainty()
+            if sample == 'entropy':
+                most_uncertain, *self.todo = self.ent()
             else:
                 most_uncertain, *self.todo = self.todo
+
             self.done += [most_uncertain]
 
             count += 1
             if count % self.batch_size == 0 or not self.todo:
                 self.scores.append(self.get_scores(self.test_set))
 
-    def calculate_uncertainty(self) -> List[Tuple[List[float], Any]]:
+    def ent(self) -> List[Tuple[List[float], Any]]:
         """
         Rank remaining TODO samples by uncertainty (entropy of predicted probabilities).
         """
@@ -85,6 +88,135 @@ class NaiveBayesModel(Model):
         scored.sort(reverse=True, key=lambda tup: tup[0])  # High entropy = high uncertainty
 
         return [sample for _, sample in scored]
+    
+    def fit_2(self, train: List[Tuple[List[float], Any]], test: List[Tuple[List[float], Any]], uncertainty = 'epistemic'):
+        """
+        Rank remaining TODO samples by uncertainty (variance in likelihood of predictions).
+        """
+        def train_models_with_perturbations(X, y, n_models=20, seed=42):
+
+            rng = np.random.RandomState(seed)
+            models = []
+            log_likelihoods = []
+
+            for _ in range(n_models):
+                X_resampled, y_resampled = resample(X, y, random_state=rng)
+                model = GaussianNB()
+                model.fit(X_resampled, y_resampled)
+
+                log_proba = model.predict_log_proba(X_resampled)
+                class_indices = {cls: idx for idx, cls in enumerate(model.classes_)}
+
+                # Compute total log-likelihood safely
+                log_likelihood = np.sum([
+                    log_proba[i, class_indices[y_resampled[i]]] for i in range(len(y_resampled))
+                ])
+
+                # Append as scalar float
+                log_likelihoods.append(float(log_likelihood))
+
+                models.append(model)
+
+            max_log_likelihood = max(log_likelihoods)
+            log_likelihoods = np.array(log_likelihoods)
+            pi_theta = np.exp(log_likelihoods - max_log_likelihood)  # normalized
+
+            return models, pi_theta
+        
+        f = lambda a : 2 * a -1
+
+        def compute_plausibility(models, pi_theta, x_instance):
+            """
+            Computes π(1|x) and π(0|x) for a given instance x.
+            """
+            min_1 = []
+            min_0 = []
+
+            for model, pi in zip(models, pi_theta):
+                prob = model.predict_proba([x_instance])[0]
+                if len(model.classes_) == 2:
+                    idx0 = np.where(model.classes_ == 0)[0][0] if 0 in model.classes_ else None
+                    idx1 = np.where(model.classes_ == 1)[0][0] if 1 in model.classes_ else None
+                    p0 = prob[idx0] if idx0 is not None else 0.0
+                    p1 = prob[idx1] if idx1 is not None else 0.0
+                else:
+                    # Only one class seen during training
+                    if model.classes_[0] == 0:
+                        p0, p1 = prob[0], 0.0
+                    else:
+                        p0, p1 = 0.0, prob[0]
+
+                min_1.append(min(pi, p1))
+                min_0.append(min(pi, p0))
+
+            pi_1 = max(min_1)
+            pi_0 = max(min_0)
+            return pi_1, pi_0
+        
+        def compute_uncertainties(models, pi_theta):
+            """
+            Computes epistemic and aleatoric uncertainties for all instances in X_test.
+            """
+            epistemic = []
+            aleatoric = []
+
+            for x, y in self.todo:
+                pi1, pi0 = compute_plausibility(models, pi_theta, x)
+                ue = min(pi1, pi0)
+                ua = 1 - max(pi1, pi0)
+                epistemic.append((ue, (x, y)))
+                aleatoric.append((ua, (x, y)))
+
+            # Sort in descending order by uncertainty
+            epistemic_sorted = sorted(epistemic, key=lambda t: t[0], reverse=True)
+            aleatoric_sorted = sorted(aleatoric, key=lambda t: t[0], reverse=True)
+
+            return epistemic_sorted, aleatoric_sorted
+        # Train ensemble of GNB models
+
+        self.test_set = test
+        self.batch_size = max(1, int(len(train) * self.batch_per))
+        shuffled = train.copy()
+        random.shuffle(shuffled)
+        self.done = shuffled[:self.start]
+        self.todo = shuffled[self.start:]
+
+        count = 0
+        
+        while self.todo and len(self.done) <= self.stop:
+            # Train model on current done set
+            X_done = [x for x, _ in self.done]
+            y_done = [y for _, y in self.done]
+            #self.model.fit(X_done, y_done)
+            models, pi_theta = train_models_with_perturbations(X_done, y_done, n_models=30)
+
+            # Get most uncertain sample
+            
+            epistemic_unc, aleatoric_unc = compute_uncertainties(models, pi_theta)
+            if uncertainty == 'epistemic':
+                most_uncertain, *self.todo = epistemic_unc
+            else:
+                most_uncertain, *self.todo = aleatoric_unc
+
+            self.done += [most_uncertain]
+
+            count += 1
+            scores = []
+            if count % self.batch_size == 0 or not self.todo:
+                for model in models:
+                    self.model = model
+                    scores.append(self.get_scores(self.test_set))
+                    
+            # Compute average of each metric
+            avg_score = {
+                "accuracy": np.mean([s["accuracy"] for s in scores]),
+                "recall": np.mean([s["recall"] for s in scores]),
+                "f1": np.mean([s["f1"] for s in scores]),
+                "false_positive_rate": np.mean([s["false_positive_rate"] for s in scores])
+            }
+
+            self.scores.append(avg_score) 
+            
 
     def get_scores(self, test: List[Tuple[List[float], Any]]) -> Dict[str, float]:
         X_test = [x for x, _ in test]
--- src/plotting.py
+++ src/plotting.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_model_metrics(accuracies_unc, recalls_unc, f1s_unc, fprs_unc,
-                       accuracies_rnd, recalls_rnd, f1s_rnd, fprs_rnd, num_repeats=10):
+                       accuracies_rnd, recalls_rnd, f1s_rnd, fprs_rnd, 
+                       accuracies_eu, recalls_eu, f1s_eu, fprs_eu,
+                       accuracies_au, recalls_au, f1s_au, fprs_au,num_repeats=10):
     """
     Plots the performance metrics (Accuracy, Recall, F1, False Positive Rate) 
     for both uncertainty and random sampling over multiple repeats.
@@ -30,42 +32,72 @@ def plot_model_metrics(accuracies_unc, recalls_unc, f1s_unc, fprs_unc,
     f1s_rnd = np.array(f1s_rnd)
     fprs_rnd = np.array(fprs_rnd)
 
+    accuracies_eu = np.array(accuracies_eu)
+    recalls_eu = np.array(recalls_eu)
+    f1s_eu = np.array(f1s_eu)
+    fprs_eu = np.array(fprs_eu)
+
+    accuracies_au = np.array(accuracies_au)
+    recalls_au = np.array(recalls_au)
+    f1s_au = np.array(f1s_au)
+    fprs_au = np.array(fprs_au)
+
     # Calculate means and variances
     mean_acc_unc = accuracies_unc.mean(axis=0)
     mean_acc_rnd = accuracies_rnd.mean(axis=0)
+    mean_acc_eu = accuracies_eu.mean(axis=0)
+    mean_acc_au = accuracies_au.mean(axis=0)
     var_acc_unc = accuracies_unc.var(axis=0)
     var_acc_rnd = accuracies_rnd.var(axis=0)
+    var_acc_eu = accuracies_eu.var(axis=0)
+    var_acc_au = accuracies_au.var(axis=0)
 
     mean_rec_unc = recalls_unc.mean(axis=0)
     mean_rec_rnd = recalls_rnd.mean(axis=0)
+    mean_acc_eu = recalls_eu.mean(axis=0)
+    mean_acc_au = recalls_au.mean(axis=0)
     var_rec_unc = recalls_unc.var(axis=0)
     var_rec_rnd = recalls_rnd.var(axis=0)
+    var_acc_eu = recalls_eu.var(axis=0)
+    var_acc_au = recalls_au.var(axis=0)
 
     mean_f1_unc = f1s_unc.mean(axis=0)
     mean_f1_rnd = f1s_rnd.mean(axis=0)
+    mean_f1_eu = f1s_eu.mean(axis=0)
+    mean_f1_au = f1s_au.mean(axis=0)
     var_f1_unc = f1s_unc.var(axis=0)
     var_f1_rnd = f1s_rnd.var(axis=0)
+    var_f1_eu = f1s_eu.var(axis=0)
+    var_f1_au = f1s_au.var(axis=0)
 
     mean_fpr_unc = fprs_unc.mean(axis=0)
     mean_fpr_rnd = fprs_rnd.mean(axis=0)
+    mean_fpr_eu = fprs_eu.mean(axis=0)
+    mean_fpr_au = fprs_au.mean(axis=0)
     var_fpr_unc = fprs_unc.var(axis=0)
     var_fpr_rnd = fprs_rnd.var(axis=0)
+    var_fpr_eu = fprs_eu.var(axis=0)
+    var_fpr_au = fprs_au.var(axis=0)
 
     # Plotting
     fig, axs = plt.subplots(2, 2, figsize=(12, 10))
     fig.suptitle('Model Metrics over Time: Uncertainty vs Random Sampling')
 
     # Accuracy
-    axs[0, 0].plot(mean_acc_unc, label='Uncertainty')
+    axs[0, 0].plot(mean_acc_unc, label='Entropy')
     axs[0, 0].plot(mean_acc_rnd, label='Random', linestyle='--')
+    axs[0, 0].plot(mean_acc_unc, label='EU')
+    axs[0, 0].plot(mean_acc_unc, label='AU')
     axs[0, 0].fill_between(range(len(mean_acc_unc)), mean_acc_unc - np.sqrt(var_acc_unc), mean_acc_unc + np.sqrt(var_acc_unc), alpha=0.2)
     axs[0, 0].fill_between(range(len(mean_acc_rnd)), mean_acc_rnd - np.sqrt(var_acc_rnd), mean_acc_rnd + np.sqrt(var_acc_rnd), alpha=0.2)
+    axs[0, 0].fill_between(range(len(mean_acc_eu)), mean_acc_eu - np.sqrt(var_acc_eu), mean_acc_eu + np.sqrt(var_acc_eu), alpha=0.2)
+    axs[0, 0].fill_between(range(len(mean_acc_au)), mean_acc_au - np.sqrt(var_acc_au), mean_acc_au + np.sqrt(var_acc_au), alpha=0.2)
     axs[0, 0].set_title('Accuracy')
     axs[0, 0].set_ylabel('Score')
     axs[0, 0].legend()
 
     # Recall
-    axs[0, 1].plot(mean_rec_unc, label='Uncertainty')
+    axs[0, 1].plot(mean_rec_unc, label='Entropy')
     axs[0, 1].plot(mean_rec_rnd, label='Random', linestyle='--')
     axs[0, 1].fill_between(range(len(mean_rec_unc)), mean_rec_unc - np.sqrt(var_rec_unc), mean_rec_unc + np.sqrt(var_rec_unc), alpha=0.2)
     axs[0, 1].fill_between(range(len(mean_rec_rnd)), mean_rec_rnd - np.sqrt(var_rec_rnd), mean_rec_rnd + np.sqrt(var_rec_rnd), alpha=0.2)
@@ -73,7 +105,7 @@ def plot_model_metrics(accuracies_unc, recalls_unc, f1s_unc, fprs_unc,
     axs[0, 1].legend()
 
     # F1
-    axs[1, 0].plot(mean_f1_unc, label='Uncertainty')
+    axs[1, 0].plot(mean_f1_unc, label='Entropy')
     axs[1, 0].plot(mean_f1_rnd, label='Random', linestyle='--')
     axs[1, 0].fill_between(range(len(mean_f1_unc)), mean_f1_unc - np.sqrt(var_f1_unc), mean_f1_unc + np.sqrt(var_f1_unc), alpha=0.2)
     axs[1, 0].fill_between(range(len(mean_f1_rnd)), mean_f1_rnd - np.sqrt(var_f1_rnd), mean_f1_rnd + np.sqrt(var_f1_rnd), alpha=0.2)
@@ -83,7 +115,7 @@ def plot_model_metrics(accuracies_unc, recalls_unc, f1s_unc, fprs_unc,
     axs[1, 0].legend()
 
     # False Positive Rate
-    axs[1, 1].plot(mean_fpr_unc, label='Uncertainty')
+    axs[1, 1].plot(mean_fpr_unc, label='Entropy')
     axs[1, 1].plot(mean_fpr_rnd, label='Random', linestyle='--')
     axs[1, 1].fill_between(range(len(mean_fpr_unc)), mean_fpr_unc - np.sqrt(var_fpr_unc), mean_fpr_unc + np.sqrt(var_fpr_unc), alpha=0.2)
     axs[1, 1].fill_between(range(len(mean_fpr_rnd)), mean_fpr_rnd - np.sqrt(var_fpr_rnd), mean_fpr_rnd + np.sqrt(var_fpr_rnd), alpha=0.2)
